dde14d704166769b84e880504decc05f918c50b1
Johnathan-Xie <65459274+Johnathan-Xie@users.noreply.github.com>
2021-02-08 14:45:50 -0800

    Update rock_paper_scissors.py

--- Day1/rock_paper_scissors.py
+++ Day1/rock_paper_scissors.py
@@ -5,7 +5,7 @@ def play(player_choice, computer_choice):
         print('Computer played ' + computer_choice + ". It's a tie.")
     
     elif (player_choice == 'rock'):
-        if(computer_choice == 'paper'):
+        if(computer_choice == 'scissors'):
             print('Computer plays scissors. You win!')
         else:
             print('Computer plays paper. You lose!')
@@ -24,8 +24,22 @@ def play(player_choice, computer_choice):
     else:
         print('invalid choice')
 
+#for students to complete
+def play_win(player_choice):
+    if player_choice == 'scissors':
+        return 'rock'
+    if player_choice == 'paper':
+        return 'scissors'
+    if player_choice == 'rock':
+        return 'paper'
+    else:
+        raise ValueError
+    #winning_values = {'scissors': 'rock', 'paper': 'scissors', 'rock': 'paper'}
+    #return winning_values[player_choice]
+
 if __name__ == '__main__':
     inp = input()
     while inp != "":
-        play(inp, random.choice(['rock', 'paper', 'scissors']))
+        #play(inp, random.choice(['rock', 'paper', 'scissors']))
+        play(inp, play_win(inp))
         inp = input()
